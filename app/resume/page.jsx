22a923382650abd1b9e83dccf20e9472ceaeaf79
monkeyit <truongvanthanh2609@gmail.com>
2025-01-04 20:53:17 +0700

    Update resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -268,7 +268,7 @@ const Resume = () => {
             <TabsContent value="skills" className="w-full h-full">
               <div className="flex flex-col gap-[30px]">
                 <div className="flex flex-col gap-[30px] text-center xl:text-left">
-                  <h3 className="text-4 font-bold">{skills.title}</h3>
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
                   <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                     {skills.description}
                   </p>
